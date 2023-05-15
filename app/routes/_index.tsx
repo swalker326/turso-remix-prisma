321ab5284a4c1bfed25a090bf2f9542203c67475
Shane <shane@swalker.dev>
2023-05-14 22:13:09 -0500

    added sample turso example

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,38 +1,33 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { json, type V2_MetaFunction } from "@remix-run/node";
+import { createClient } from "@libsql/client";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
+export async function loader() {
+  const client = createClient({
+    url: "libsql://918577d0f12e38-secure-beach-head-swalker326.turso.io",
+    authToken: process.env.TURSO_API_KEY
+  });
+  const users = await client.execute("select * from example_users");
+  return json(users);
+}
+
 export default function Index() {
+  const { rows } = useLoaderData();
+  // console.log("users", users.rows);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      {rows.map((user: any) => {
+        return (
+          <div key={user.uid}>
+            <h2>{user.email}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
